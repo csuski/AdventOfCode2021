9b9c469ba92bcc717a4116b53e215303cad84721
Christopher Suski <1329211+csuski@users.noreply.github.com>
2021-12-01 20:32:41 -0700

    Add project files.

---
+++ Day1/Program.fs
@@ -0,0 +1,28 @@
+﻿let countIncrease (x, y) = match x < y with | true -> 1 | false -> 0
+
+let rec countIncreases = function 
+    | [] -> 0
+    | _::[] -> 0
+    | a::b::t -> countIncrease(a, b) + countIncreases(b::t)
+
+// read all lines of a file, convert to string list, map to int list
+open System.IO
+let intList = File.ReadAllLines(@"input.txt") |> Seq.toList |> List.map int
+ 
+let numberOfIncrease = intList |> countIncreases
+
+printf "Part 1 answer %i\r\n" numberOfIncrease
+
+
+let rec sumOfThree = function 
+    | [] -> []
+    | _::[] -> []
+    | _::_::[] -> []
+    | a::b::c::t -> (a+b+c)::sumOfThree(b::c::t)
+
+
+let numberofThreeIncreases = intList |> sumOfThree |> countIncreases
+
+printf "Part 2 answer %i\r\n" numberofThreeIncreases
+
+
---
+++ Day2/Program.fs
@@ -0,0 +1,3 @@
+﻿// read all lines of a file, convert to string list, map to int list
+open System.IO
+let intList = File.ReadAllLines(@"input.txt") |> Seq.toList |> List.map int
