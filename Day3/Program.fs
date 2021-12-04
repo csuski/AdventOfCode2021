cbf2a8a28dcf28ca9744d6f89988641532ddaf69
Christopher Suski <1329211+csuski@users.noreply.github.com>
2021-12-03 23:56:29 -0700

    Day 3 part 1 Start of Day 4 part 1 and Day 3 part 2

---
+++ Day3/Program.fs
@@ -0,0 +1,53 @@
+﻿
+
+open System.IO
+let input = File.ReadAllLines(@"test.txt") |> Seq.toList
+
+let bits = input.Item(0).Length;
+let half = input.Length / 2;
+
+
+let countOfOnes = Array.create bits 0
+
+for i in 0 .. input.Length - 1 do
+    for j in 0 .. bits - 1 do
+        if input[i][j] = '1' then Array.set countOfOnes j (countOfOnes[j] + 1)
+
+
+
+let gammaCheck n = n > half
+let epsilonCheck n = n < half
+// I guess we are assuming nothing is exactly equal
+
+
+let gammaRate = List.fold (fun acc elem -> 
+    match gammaCheck elem with
+    | true -> acc <<< 1 ||| 1
+    | false -> acc <<< 1) 0 (Seq.toList countOfOnes)
+
+let epsilonRate = List.fold (fun acc elem -> 
+    match epsilonCheck elem with
+    | true -> acc <<< 1 ||| 1
+    | false -> acc <<< 1) 0 (Seq.toList countOfOnes)
+
+let result = gammaRate * epsilonRate
+
+printf "Part 1 answer %A\r\n" result
+
+
+let oxygenBitCriteria index (value:string) = 
+    if gammaCheck countOfOnes[index] then value[index] = '1'
+    else value[index] <> '1'
+
+let rec oxygenFilter (list:string list) index = 
+    match list.Length with 
+    | 0 -> list
+    | 1 -> list
+    | _ -> oxygenFilter (List.filter (oxygenBitCriteria index) list) (index + 1)
+
+    //if list.length = 1 then list
+    //else List.filter (oxygenBitCriteria index) list
+
+let oxygenBinary = oxygenFilter input 0
+
+printf "Part 2 answer %A\r\n" oxygenBinary
---
+++ Day4/Program.fs
@@ -0,0 +1,29 @@
+﻿
+type Square = { value:string; column:int; row:int; draw:int }
+
+type Board = { squares:List<Square>}
+
+
+
+
+
+open System.IO
+let input = File.ReadAllLines(@"input.txt")
+
+
+
+let numbersDrawn = input[0].Split ',' |> Seq.toList
+let boardsInput = input[1..]
+
+
+let splitSpace (row:string) = row.Split ' '
+
+
+// while readline !+ null
+// read f lines
+
+
+
+let result = 0
+
+printf "Part 1 answer %A\r\n" result
